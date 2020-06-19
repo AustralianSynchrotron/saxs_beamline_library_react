1afeed60774de1a735dee2e714e7c189b594c89d
Stephen Mudie <stephenm@ansto.gov.au>
2020-06-19 12:02:24 +1000

    Brackets in wrong spot...

--- src/components/q_range_change_page/q_range_change_page.js
+++ src/components/q_range_change_page/q_range_change_page.js
@@ -131,7 +131,7 @@ const QRangeChange = (props) => {
   };
 
   const handleStartCameraLengthChange = () => {
-    dispatch(clearProgressLog)();
+    dispatch(clearProgressLog());
     dispatch(changeCameraLength(cameraLength));
   };
 
